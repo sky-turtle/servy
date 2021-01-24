99b82c94ff251103c4ac77f52faaec5e4078937c
Leo Mendoza Jr <leo@ok3.dev>
2021-01-24 18:37:32 -0500

    finish lesson 8 video. pick up at notes and exercises

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -3,9 +3,41 @@ defmodule Servy.Handler do
     request
     |> parse()
     |> log
+    |> rewrite_path
     |> route()
+    |> track()
+    |> emojify()
     |> format_response()
   end
+
+  # Thought Experiment: other useful function?
+  # check if user is authenticated, check if the url is valid
+  def emojify(%{status: 200} = conv) do
+    %{conv | resp_body: "🚀 #{conv.resp_body} 🚀"}
+  end
+
+  def emojify(conv), do: conv
+
+  def track(%{status: 404, path: path} = conv) do
+    IO.puts("Warning #{path} is on the loose!")
+    conv
+  end
+
+  def track(conv), do: conv
+
+  def rewrite_path(%{path: path} = conv) do
+    regex = ~r{\/(?<animal>\w+)\?id=(?<id>\d+)}
+    captures = Regex.named_captures(regex, path)
+    rewrite_path_captures(conv, captures)
+  end
+
+  def rewrite_path(conv), do: conv
+
+  def rewrite_path_captures(conv, %{"animal" => animal, id: id}) do
+    %{conv | path: "/#{animal}/#{id}"}
+  end
+
+  def rewrite_path_captures(conv, nil), do: conv
 
   def log(conv), do: IO.inspect(conv)
 
@@ -36,8 +68,8 @@ defmodule Servy.Handler do
     %{conv | status: 200, resp_body: "Bear #{id}"}
   end
 
-  def route(%{method: "DELETE", path: "/bears/" <> id} = conv) do
-    %{conv | status: 200, resp_body: "Deleted Bear #{id}"}
+  def route(%{method: "DELETE", path: "/bears/" <> _id} = conv) do
+    %{conv | status: 403, resp_body: "Bears must never be deleted"}
   end
 
   def route(%{path: path} = conv) do
@@ -119,4 +151,29 @@ Accept: */*
 """
 
 response = Servy.Handler.handle(request_bear_1_delete)
+IO.puts(response)
+
+response = Servy.Handler.handle(request_bear_1)
+IO.puts(response)
+
+request_bear_1_delete = """
+GET /wildlife HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request_bear_1_delete)
+IO.puts(response)
+
+request = """
+GET /bears?id=1 HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
 IO.puts(response)
