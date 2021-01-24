7897881a05e0fad9f3f63db371a87a32683fee97
Leo Mendoza Jr <leo@ok3.dev>
2021-01-24 12:11:43 -0500

    complete lesson 7. Add route for DELETE.

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -2,32 +2,71 @@ defmodule Servy.Handler do
   def handle(request) do
     request
     |> parse()
+    |> log
     |> route()
     |> format_response()
   end
 
+  def log(conv), do: IO.inspect(conv)
+
   def parse(request) do
-    first_line = String.split(request, "\n") |> List.first() |> String.split(" ")
-    [method, path, _] = first_line
-    %{method: method, path: path, resp_body: ""}
+    [method, path, _version] =
+      request
+      |> String.split("\n")
+      |> List.first()
+      |> String.split(" ")
+
+    %{
+      method: method,
+      path: path,
+      resp_body: "",
+      status: nil
+    }
   end
 
-  def route(conv) do
-    %{method: "GET", path: "/wildthings", resp_body: "Bears, Lions, Tigers"}
+  def route(%{method: "GET", path: "/wildthings"} = conv) do
+    %{conv | status: 200, resp_body: "Bears, Lions, Tigers"}
+  end
+
+  def route(%{method: "GET", path: "/bears"} = conv) do
+    %{conv | status: 200, resp_body: "Smokey, Teddy, Boobear"}
+  end
+
+  def route(%{method: "GET", path: "/bears/" <> id} = conv) do
+    %{conv | status: 200, resp_body: "Bear #{id}"}
+  end
+
+  def route(%{method: "DELETE", path: "/bears/" <> id} = conv) do
+    %{conv | status: 200, resp_body: "Deleted Bear #{id}"}
+  end
+
+  def route(%{path: path} = conv) do
+    %{conv | status: 404, resp_body: "No #{path} here!"}
   end
 
   def format_response(conv) do
     """
-    HTTP/1.1 200 OK
+    HTTP/1.1 #{conv.status} #{status_reason(conv.status)}
     Content-Type: text/html
-    Content-Length: 20
+    Content-Length: #{byte_size(conv.resp_body)}
 
-    Bears, Lions, Tigers
+    #{conv.resp_body}
     """
+  end
+
+  defp status_reason(code) do
+    %{
+      200 => "OK",
+      201 => "Created",
+      401 => "Unauthorized",
+      403 => "Forbidden",
+      404 => "Not Found",
+      500 => "Internal Server Error"
+    }[code]
   end
 end
 
-request = """
+request_wildthings = """
 GET /wildthings HTTP/1.1
 Host: example.com
 User-Agent: ExampleBrowser/1.0
@@ -35,6 +74,49 @@ Accept: */*
 
 """
 
+response = Servy.Handler.handle(request_wildthings)
+IO.puts(response)
 
-response = Servy.Handler.handle(request)
+request_bears = """
+GET /bears HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request_bears)
+IO.puts(response)
+
+request_bigfoot = """
+GET /bigfoot HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request_bigfoot)
+IO.puts(response)
+
+request_bear_1 = """
+GET /bears/1 HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request_bear_1)
+IO.puts(response)
+
+request_bear_1_delete = """
+DELETE /bears/1 HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request_bear_1_delete)
 IO.puts(response)
