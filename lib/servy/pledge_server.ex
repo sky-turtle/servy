9f5a08b6c77c1adf3ccb340560284718861433b4
Leo Mendoza Jr <leo@skyturtle.io>
2021-02-26 10:57:23 -0500

    attempt implement total_pledged/0

--- lib/servy/pledge_server.ex
+++ lib/servy/pledge_server.ex
@@ -1,30 +1,73 @@
 defmodule Servy.PledgeServer do
-  def listen_loop(state) do
-    IO.puts("\nWaiting for a message...")
+  @name :pledge_server
+
+  # Client interface functions
+  def start do
+    IO.puts("Starting the pledge server...")
+    pid = spawn(__MODULE__, :listen_loop, [[]])
+    Process.register(pid, @name)
+    pid
+  end
+
+  def create_pledge(name, amount) do
+    send(@name, {self(), :create_pledge, name, amount})
 
     receive do
-      {:create_pledge, name, amount} ->
-        new_state = [{name, amount} | state]
-        IO.puts("#{name} made a pledge for $#{amount} dollars")
-        IO.puts("#{inspect(new_state)}")
+      {:response, status} -> status
+    end
+  end
+
+  def recent_pledges() do
+    send(@name, {self(), :recent_pledges})
+
+    receive do
+      {:response, pledges} ->
+        IO.inspect(pledges)
+    end
+  end
+
+  def total_pledged() do
+    send(@name, {self(), :recent_pledges})
+
+    receive do
+      {:response, pledges} ->
+        Enum.reduce(pledges, 0, fn {_name, amount}, acc -> amount + acc end)
+    end
+  end
+
+  # Server
+
+  def listen_loop(state) do
+    receive do
+      {sender, :create_pledge, name, amount} ->
+        {:ok, id} = send_pledge_to_service(name, amount)
+        recent_state = Enum.take(state, 2)
+        new_state = [{name, amount} | recent_state]
+        send(sender, {:response, id})
         listen_loop(new_state)
 
       {sender, :recent_pledges} ->
         send(sender, {:response, state})
-        IO.puts("Sent pledges to #{inspect(sender)}")
         listen_loop(state)
     end
   end
-
-  # def create_pledge(name, amount) do
-  #   {:ok, id} = send_pledge_to_service(name, amount)
-  # end
-
-  # def recent_pledges() do
-  # end
 
   defp send_pledge_to_service(_name, _amount) do
     # This code would send the pledge to an external service
     {:ok, "pledge-#{:rand.uniform(1000)}"}
   end
 end
+
+alias Servy.PledgeServer
+
+pid = PledgeServer.start()
+
+IO.inspect(PledgeServer.create_pledge("larry", 10))
+IO.inspect(PledgeServer.create_pledge("moe", 20))
+IO.inspect(PledgeServer.create_pledge("curly", 30))
+IO.inspect(PledgeServer.create_pledge("daisy", 40))
+IO.inspect(PledgeServer.create_pledge("grace", 50))
+
+IO.inspect(PledgeServer.recent_pledges())
+
+IO.inspect(PledgeServer.total_pledged())
